101f9cdd15a3cc35c07a5499fdcc3e3fa98774f0
Mino <aminemahi.pro@gmail.com>
2025-11-20 12:07:09 +0100

    text presentation

---
+++ src/section/Presentation.jsx
@@ -0,0 +1,17 @@
+import "./Presentation.css"
+
+export default function Presentation(){
+    return(
+        <section className="presentation">
+            <img src="" alt="profil picture" />
+            <p>
+                Notre mission est de créer des expériences digitales uniques et performantes. <br />
+                Nous accompagnons les entreprises, startups et entrepreneurs dans le développement de solutions web modernes,
+                 le design d’interfaces intuitives et la création d’applications sur-mesure. <br />
+                Notre approche repose sur la qualité, l’innovation et l’écoute — 
+                pour que chaque projet devienne un véritable outil de croissance.
+
+            </p>
+        </section>
+    )
+}
