a286678aacf17153d84c67ac5cc08fb99e9afa63
Mino <aminemahi.pro@gmail.com>
2025-11-21 12:17:22 +0100

    modern blob and better responsive

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,5 +1,6 @@
 import "./Hero.css";
-import Mac from "../assets/black-mac.jpg"
+import Mac from "../assets/black-mac.jpg";
+import Blob from "../assets/modern-blob.png";
 
 export default function Hero(){
     return(
@@ -8,6 +9,7 @@ export default function Hero(){
                 <h1>MINO PRODUCTIONS</h1>
                 <h2>By Amine MAHI</h2>
             </div>
+            <img src={Blob} alt="Modern blob" className="hero-blob"/>
             <img src={Mac} alt="Black Macbook" className="hero-mac" />
             
         </section>
